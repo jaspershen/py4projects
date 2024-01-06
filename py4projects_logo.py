566dda1b784c4ca8fdcc8976519c01fd45872125
jaspershen <shenxt1990@outlook.com>
2024-01-06 14:13:33 -0800

    update

---
+++ py4projects_logo.py
@@ -0,0 +1,21 @@
+def py4projects_logo(py4projects_logo, update_date):
+    py4projects_version = "1.0"  # version
+    update_date = "2024-01-06"    # date
+    print("Thank you for using py4projects!")
+    print(f"Version {py4projects_version} ({update_date})")
+    logo = [
+        "           d8888                            d8b                   888             ",
+        "          d8P888                            Y8P                   888             ",
+        "         d8P 888                                                  888             ",
+        "888d888 d8P  888  88888b.  888d888 .d88b.  8888  .d88b.   .d8888b 888888 .d8888b  ",
+        "888P\"  d88   888  888 \"88b 888P\"  d88\"\"88b \"888 d8P  Y8b d88P\"    888    88K      ",
+        "888    8888888888 888  888 888    888  888  888 88888888 888      888    \"Y8888b. ",
+        "888          888  888 d88P 888    Y88..88P  888 Y8b.     Y88b.    Y88b.       X88 ",
+        "888          888  88888P\"  888     \"Y88P\"   888  \"Y8888   \"Y8888P  \"Y888  88888P' ",
+        "                  888                       888                                   ",
+        "                  888                      d88P                                   ",
+        "                  888                    888P\"                                    "
+    ]
+    print("\n".join(logo))
+
+# py4projects_logo("1.0", "2024-01-06")
